5255e02934aa1ce0ec2c11da377953c5b7f5b954
VladimirJG <vladimi85@gmail.com>
2022-05-13 10:49:09 +0300

    mas

---
+++ SM.js
@@ -0,0 +1,43 @@
+/* var myTopThreeCars = ["Mazda", "Toyota", "Opel_TD"]
+console.log(myTopThreeCars)
+
+var cars = [
+    "Mazda",
+    "Toyota",
+    "Opel",
+    "Lexus",
+    "Suzuky",
+    "BMW",
+    "Kia",
+    "Henday",
+    "Bently"
+]
+console.log(cars) */
+
+/* var cars = [
+    "Mazda",
+    "Toyota",
+    "Opel",
+    "Lexus",
+    "Suzuky",
+    "BMW",
+    "Kia",
+    "Henday",
+    "Bently"
+]
+console.log(cars[2], cars[5])
+cars[2] = "Mersedes"
+console.log(cars) */
+
+var cars = []
+cars[0] = 'Mazda'
+cars[1] = 'Mersedes'
+cars[2] = 'Suzuky'
+cars[3] = 'Kia'
+cars[4] = 'Bently'
+cars[5] = 'Toyota'
+cars[6] = 'Lexus'
+cars[7] = 'BMW'
+cars[8] = 'Henday'
+
+console.log(cars)
