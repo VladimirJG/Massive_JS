5f2f732a09e0a23837ee32bdc87966b0f3d49674
VladimirJG <vladimi85@gmail.com>
2022-05-13 11:23:38 +0300

    mas

--- SM.js
+++ SM.js
@@ -29,7 +29,7 @@ console.log(cars[2], cars[5])
 cars[2] = "Mersedes"
 console.log(cars) */
 
-var cars = []
+/* var cars = []
 cars[0] = 'Mazda'
 cars[1] = 'Mersedes'
 cars[2] = 'Suzuky'
@@ -39,5 +39,22 @@ cars[5] = 'Toyota'
 cars[6] = 'Lexus'
 cars[7] = 'BMW'
 cars[8] = 'Henday'
+cars[55] = 'Horse'
 
-console.log(cars)
+console.log(cars) */
+
+
+/* var carsAndNumbers = [7, "cars", ['toyota', 'mers', 'horse', 2598.5], 13]
+console.log(carsAndNumbers[2][3], carsAndNumbers[3]) */
+
+/* var maniacs = ['Vanko', 'Danko', 'Sranko']
+console.log(maniacs.length)
+console.log(maniacs[maniacs.length - 1]) */
+
+var animals = []
+animals.push('cat')
+animals.push('dog')
+animals.push('horse')
+console.log(animals)
+console.log(animals.length)
+console.log(animals[animals.length - 1])
