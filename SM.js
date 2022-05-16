1dd4e13ec7e7e46d35fdcd9637435f7f2043378e
VladimirJG <vladimi85@gmail.com>
2022-05-16 08:46:54 +0300

    mas

--- SM.js
+++ SM.js
@@ -186,8 +186,16 @@ console.log(number)
 console.log(Math.floor(number)) */
 
 
-var randomWords = ['Взрыв', 'Пещера', 'Принцесса', 'Карандаш']
-    /* var randomIndex = Math.floor(Math.random() * 4)
+/* var randomWords = ['Взрыв', 'Пещера', 'Принцесса', 'Карандаш']
+    var randomIndex = Math.floor(Math.random() * 4)
     console.log(randomWords[randomIndex])
-     */
+    
 console.log(randomWords[Math.floor(Math.random()) * 3])
+
+
+var randomIndex = Math.floor(Math.random() * 4)
+console.log(randomWords[randomIndex]) */
+
+var cars = ['toyota', 'bmw', 'suzuki', 'lexus', 'mazerraty', 'honda']
+var indexCar = Math.floor(Math.random() * 6)
+console.log(cars[indexCar])
