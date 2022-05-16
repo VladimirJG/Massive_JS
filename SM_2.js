2505e8a5fdf48c5c41d45922ce460b2ba7748b1f
VladimirJG <vladimi85@gmail.com>
2022-05-16 09:47:04 +0300

    mas

--- SM_2.js
+++ SM_2.js
@@ -1,5 +1,5 @@
 //Программа случайного выбора вариантов
-var phrases = [
+/* var phrases = [
         'Звучит неплохо',
         'Да, это определенно надо сделать',
         'Не думаю, что это хорошая идея',
@@ -19,4 +19,15 @@ var lastCommand = phrases.pop()
 phrases.unshift(lastCommand)
 console.log(phrases.length)
 console.log(phrases[Math.floor(Math.random() * 8)])
-console.log(phrases)
+console.log(phrases) */
+
+
+var things = ['мяч', 'велосипед', 'скейт', 'наряд', 'вейп', 'байк']
+var colors = ['красный', 'синий', 'зеленый', 'белый', 'черный', 'фиолетовый']
+var values = ['крутой', 'роскошный', 'стремный', 'простой', 'улетный']
+var thing = things[Math.floor(Math.random() * 6)]
+var color = colors[Math.floor(Math.random() * 6)]
+var value = values[Math.floor(Math.random() * 5)]
+var value_2 = values[Math.floor(Math.random() * 5)]
+var phrase = 'Твой ' + color + ' ' + thing + ' такой ' + value + ' и ' + value_2 + '!!!'
+console.log(phrase)
