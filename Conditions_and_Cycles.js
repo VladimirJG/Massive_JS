1651442338a6471d25c223fe33116fd7953869ea
VladimirJG <vladimi85@gmail.com>
2022-05-19 08:00:34 +0300

    виселица

--- Conditions_and_Cycles.js
+++ Conditions_and_Cycles.js
@@ -105,7 +105,7 @@ while (randomString.length < 10) {
 } */
 
 
-var input = "javascript is awesome"
+/* var input = "javascript is awesome"
 var output = ""
 for (let i = 0; i < input.length; i++) {
 
@@ -122,24 +122,5 @@ for (let i = 0; i < input.length; i++) {
     }
 }
 
-console.log(output)
-
-
-/* var input = "javascript is awesome";
-var output = "";
-
-for (var i = 0; i < input.length; i++) {
-    if (input[i] === "a") {
-        output += "4";
-    } else if (input[i] === "e") {
-        output += "3";
-    } else if (input[i] === "i") {
-        output += "1";
-    } else if (input[i] === "o") {
-        output += "0";
-    } else {
-        output += input[i];
-    }
-}
 
 console.log(output) */
---
+++ Виселица.js
@@ -0,0 +1,15 @@
+var words = [
+    "программа",
+    "макака",
+    "прекрасный",
+    "оладушек"
+];
+var word = words[Math.floor(Math.random() * words.length)]
+
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+var remainingLetters = word.length
+
+console.log(answerArray.join(' '))
