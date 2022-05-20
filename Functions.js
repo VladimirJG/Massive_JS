0e3087a43064ccda8e0a2629711a1ccdf998385a
VladimirJG <vladimi85@gmail.com>
2022-05-20 08:33:20 +0300

    function

--- Functions.js
+++ Functions.js
@@ -15,8 +15,32 @@ console.log(sayHelloTo('pups')) */
 }
 console.log(cars(17)) */
 
-let girls = function(howManyTime) {
+/* let girls = function(howManyTime) {
     for (i = 0; i < howManyTime; i++)
         console.log('Olga ' + howManyTime + ' get up')
 }
-console.log(girls(5))
+console.log(girls(5)) */
+
+/* let people = function(howMuch, whou) {
+    for (i = 0; i < howMuch; i++) {
+        console.log(i + ' ' + whou)
+    }
+}
+console.log(people(5, 'good')) */
+
+
+/* 
+let movies = function(how, norm) {
+    for (i = 0; i < how; i++) {
+        console.log(i + ' ' + norm)
+    }
+}
+console.log(movies(8, 'godno'))
+console.log(movies(3, '^-^')) */
+
+
+
+let zadacha = function(number) {
+    return number * 2
+}
+console.log(zadacha(3))
