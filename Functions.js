92acc00be29ed1983107b4cd8824eb1f7a4eaac1
VladimirJG <vladimi85@gmail.com>
2022-05-20 07:41:22 +0300

    function

--- Functions.js
+++ Functions.js
@@ -1,4 +1,22 @@
-let firstFunction = function() {
+/* let firstFunction = function() {
     console.log('Hi, world!')
 }
-console.log(firstFunction())
+console.log(firstFunction()) */
+
+
+/* let sayHelloTo = function(name) {
+    console.log("Hi " + name + '!')
+}
+console.log(sayHelloTo('pups')) */
+
+/* let cars = function(howManyTime) {
+    for (var i = 0; i < howManyTime; i++)
+        console.log(i + ' mars')
+}
+console.log(cars(17)) */
+
+let girls = function(howManyTime) {
+    for (i = 0; i < howManyTime; i++)
+        console.log('Olga ' + howManyTime + ' get up')
+}
+console.log(girls(5))
