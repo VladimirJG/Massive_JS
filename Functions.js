460ad0be4381eefe99fe245ef4f8ca4ba5b57063
VladimirJG <vladimi85@gmail.com>
2022-05-20 12:21:28 +0300

    function

--- Functions.js
+++ Functions.js
@@ -59,19 +59,56 @@ console.log(pickRandomCars(randomCars)); */
 var randomCars = ["Toyota", "Opel", "Mazda", "Kia"]
 console.log(pickRandomCars(randomCars)) */
 
-var pR_Cars = function(tryz_lya_lya) {
+/* var pR_Cars = function(tryz_lya_lya) {
     return tryz_lya_lya[Math.floor(Math.random() * tryz_lya_lya.length)]
 }
 var randomCars = ["Toyota", "Opel", "Mazda", "Kia"]
 console.log(pR_Cars(randomCars))
+ */
 
+/// здесь был глюк(пока не скопировал код из книги - не работало)
 
-
-
-
-
-let pickRandomBalls = function(ball) {
+/* let pickRandomBalls = function(ball) {
     return ball[Math.floor(Math.random() * ball.length)]
 }
-let randomBall = ['red', 'blue', 'yellow', 'white']
+let randomBall = ['Red', 'Blue', 'Yellow', 'White']
 console.log(pickRandomBalls(randomBall))
+console.log(pickRandomBalls(['Red', 'Blue', 'Yellow', 'White'])) */
+
+
+
+/* let randomBodyParts = ['глаз', 'нос', 'рот', 'ухо']
+let randomAdjectives = ['дурацкая', 'унылая', 'вонючая']
+let randomWords = ['муха', 'ведьма', 'дубина', 'выдра', 'крыса'] */
+
+/* let rB_part = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)]
+let rA = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)]
+let rW = randomWords[Math.floor(Math.random() * randomWords.length)]
+
+let result = 'у тебя ' + rB_part + ' словно ' + rA + ' ' + rW + '!!!'
+console.log(result) */
+
+/* let pickRanmdoWord = function(randomWords) {
+    return randomWords[Math.floor(Math.random() * randomWords.length)]
+}
+ */
+let randomString = 'у тебя ' + pickRanmdoWord(randomBodyParts) + ' словно ' + pickRanmdoWord(randomAdjectives) + ' ' + pickRanmdoWord(randomWords) + '!!!'
+    /* console.log(randomString) */
+
+
+//вариант 3
+
+let randomDraznilka = function() {
+    let randomBodyParts = ['глаз', 'нос', 'рот', 'ухо'];
+    let randomAdjectives = ['дурацкая', 'унылая', 'вонючая'];
+    let randomWords = ['муха', 'ведьма', 'дубина', 'выдра', 'крыса'];
+    let pickRanmdoWord = function(randomWords) {
+        return randomWords[Math.floor(Math.random() * randomWords.length)]
+    }
+    let randomString = 'у тебя ' + pickRanmdoWord(randomBodyParts) + ' словно ' + pickRanmdoWord(randomAdjectives) + ' ' + pickRanmdoWord(randomWords) + '!!!'
+
+    return randomString
+}
+
+
+console.log(randomDraznilka)
